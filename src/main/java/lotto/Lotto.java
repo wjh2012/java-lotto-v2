a1542121523e50371174f999d230b13165a1abdb
wjh2012 <wjh2012@naver.com>
2022-12-14 18:24:23 +0900

    feat(Lotto): 정적 상수 분리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,11 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+
+    private static final int NUMBER_SIZE = 6;
+    private static final int NUMBER_START_INCLUSIVE = 1;
+    private static final int NUMBER_END_INCLUSIVE = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
